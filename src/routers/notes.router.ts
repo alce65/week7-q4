3569aa9b9466503415750be11d01f7091cca8f88
Alejandro Cerezo <alce65@hotmail.es>
2023-11-23 14:01:28 +0100

    Add JWT with a authorization interceptor

--- src/routers/notes.router.ts
+++ src/routers/notes.router.ts
@@ -2,6 +2,7 @@ import { Router as createRouter } from 'express';
 import { NotesController } from '../controllers/notes.controller.js';
 import createDebug from 'debug';
 import { NotesMongoRepo } from '../repos/notes/notes.mongo.repo.js';
+import { AuthInterceptor } from '../middleware/auth.interceptor.js';
 
 const debug = createDebug('W7E:notes:router');
 
@@ -10,10 +11,15 @@ debug('Starting');
 
 const repo = new NotesMongoRepo();
 const controller = new NotesController(repo);
+const interceptor = new AuthInterceptor();
 
 notesRouter.get('/', controller.getAll.bind(controller));
 notesRouter.get('/search', controller.search.bind(controller));
 notesRouter.get('/:id', controller.getById.bind(controller));
-notesRouter.post('/', controller.create.bind(controller));
+notesRouter.post(
+  '/',
+  interceptor.authorization.bind(interceptor),
+  controller.create.bind(controller)
+);
 notesRouter.patch('/:id', controller.update.bind(controller));
 notesRouter.delete('/:id', controller.delete.bind(controller));
